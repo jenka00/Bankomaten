cc395c8a87e8d9daa810b1ca64eeb9e8f89701bc
Jenny Kallberg <jenka00@hotmail.com>
2021-10-10 11:15:07 +0200

    ShowAccount metod

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -8,49 +8,42 @@ namespace Bankomat
     {
         static void Main(string[] args)
         {
-            int i = 0;
-
-            string[,] users = new string[5, 5];
+            string[,] users = new string[5, 6];
 
             //Användare 1
-            users[0, 0] = "Jenny"; //Namn
+            users[0, 0] = "JENNY"; //Namn
             users[0, 1] = "1234";//Lösenord
-            users[0, 2] = "Lönekonto"; //Bankkonto 1
-            users[0, 3] = "Sparkonto"; //Bankkonto 2
+            users[0, 2] = "Lönekonto Jenny"; //Bankkonto 1            
+            users[0, 3] = "50000"; //Summa konto 1
+            users[0, 4] = "Sparkonto Jenny"; //Bankkonto 2
+            users[0, 5] = "500000"; //Summa konto 2
 
             //Användare 2
-            users[1, 0] = "Thomas"; //Namn
+            users[1, 0] = "THOMAS"; //Namn
             users[1, 1] = "8181";//Lösenord
             users[1, 2] = "Lönekonto"; //Bankkonto 1
             users[1, 3] = "Sparkonto"; //Bankkonto 2
 
             //Användare 3
-            users[2, 0] = "Alma"; //Namn
+            users[2, 0] = "ALMA"; //Namn
             users[2, 1] = "1212";//Lösenord
             users[2, 2] = "Lönekonto"; //Bankkonto 1
             users[2, 3] = "Sparkonto"; //Bankkonto 2
 
             //Användare 4
-            users[3, 0] = "Viktor"; //Namn
+            users[3, 0] = "VIKTOR"; //Namn
             users[3, 1] = "1313";//Lösenord
             users[3, 2] = "Lönekonto"; //Bankkonto 1
             users[3, 3] = "Sparkonto"; //Bankkonto 2
 
             //Användare 5
-            users[4, 0] = "Felix"; //Namn
+            users[4, 0] = "FELIX"; //Namn
             users[4, 1] = "1818";//Lösenord
             users[4, 2] = "Lönekonto"; //Bankkonto 1
             users[4, 3] = "Sparkonto"; //Bankkonto 2
 
             Console.WriteLine("Skriv ditt användarnamn");
             string userName = Console.ReadLine().ToUpper();
-
-            while (userName != users[i, 0].ToUpper())
-            {
-                Console.Clear();
-                Console.WriteLine("Okänd användare. Försök igen.");
-                userName = Console.ReadLine().ToUpper();
-            }
 
             if (LogIn(users, userName))
 
@@ -63,7 +56,7 @@ namespace Bankomat
                 Environment.Exit(0); //Stänger ner programmet
             }
 
-           bool loop = true;
+            bool loop = true;
             int menuChoice = 0;
             do
             {
@@ -106,7 +99,7 @@ namespace Bankomat
             int tryPass = 0;
 
             while (tryPass < 3)
-            { 
+            {
                 Console.WriteLine("Skriv ditt lösenord");
 
                 string password = Console.ReadLine();
@@ -116,9 +109,9 @@ namespace Bankomat
                 {
                     if (users[i, 1] == password)
                     {
-                        if (users[i, 0].ToUpper() == userName)
+                        if (users[i, 0] == userName)
                         {
-                            isChecking = true; 
+                            isChecking = true;
                             tryPass = 3;
                             break;
                         }
@@ -126,10 +119,10 @@ namespace Bankomat
                         else
                         {
                             Console.WriteLine("Fel lösenord.");
-                            
-                            break;    
+
+                            break;
                         }
-                    }                  
+                    }
                 }
                 tryPass++;
             }
@@ -137,16 +130,22 @@ namespace Bankomat
             {
                 return true;
             }
-
+            else
             {
                 return false;
             }
         }
-        
+
         static void ShowAccount(string[,] users, string userName)
         {
-            
-            Console.WriteLine("Tillgängliga konton;");
+            for (int i = 0; i < users.GetLength(0); i++)
+            {
+                if (users[i, 0] == userName)
+                {
+                    Console.WriteLine("Tillgängliga konton: \n{0}: {1}kr \n{2}: {3}kr", users[i, 2], users[i, 3], users[i, 4], users[i, 5]);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
